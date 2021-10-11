b07d7f699abc7f47bd326f986a1572d6a4ecc257
miao <miao@163.com>
2021-10-11 11:30:49 +0800

    完成js功能

---
+++ src/main.js
@@ -0,0 +1,78 @@
+import string from './css'
+
+const audio = new Audio('music/PapaMamaLocaPipa.m4a')
+const player = {
+  id: undefined,
+  time: 100,
+  ui: {
+    demo: document.querySelector('.code'),
+    demo2: document.querySelector('.minionStyle'),
+    codeArea: document.querySelector('.codeArea')
+  },
+  events: {
+    '.btnPause': 'pause',
+    '.btnPlay': 'play',
+    '.btnSlow': 'slow',
+    '.btnNormal': 'normal',
+    '.btnFast': 'fast',
+    '.btnMusic': 'music'
+  },
+  n: 1,
+  init: () => {
+    player.ui.demo.innerText = string.substr(0, player.n)
+    player.ui.demo2.innerHTML = string.substr(0, player.n)
+    player.bindEvents()
+    player.play()
+  },
+  bindEvents: () => {
+    for (let key in player.events) {
+      if (player.events.hasOwnProperty(key)) {
+        const value = player.events[key] // pause / play / slow
+        document.querySelector(key).addEventListener('click', player[value])
+      }
+    }
+    
+  },
+  run: (event) => {
+    console.log(event)
+    player.n += 1
+    if (player.n > string.length) {
+      window.clearInterval(player.id)
+      return
+    }
+    player.ui.demo.innerText = string.substr(0, player.n)
+    player.ui.demo2.innerHTML = string.substr(0, player.n)
+    player.ui.codeArea.scrollTop = player.ui.demo.scrollHeight
+  },
+  play: () => {
+    window.clearInterval(player.id)
+    player.id = setInterval(player.run, player.time)
+  },
+  pause: () => {
+    window.clearInterval(player.id)
+  },
+  slow: () => {
+    player.pause()
+    player.time = 300
+    player.play()
+  },
+  normal: () => {
+    player.pause()
+    player.time = 100
+    player.play()
+  },
+  fast: () => {
+    player.pause()
+    player.time = 0
+    player.play()
+  },
+  music: () => {
+    if (audio.paused){
+      audio.play().then()
+    }else {
+      audio.pause()
+    }
+  }
+}
+
+player.init()
